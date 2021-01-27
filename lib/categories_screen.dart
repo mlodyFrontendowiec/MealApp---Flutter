de0f5932342bb8e7ada59c7dbf31f4ce526fae4a
mlodyFrontendowiec <pawel.kruszelnicki@onet.pl>
2021-01-27 20:20:47 +0100

    Add routes

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -13,9 +13,10 @@ class CategoriesScreen extends StatelessWidget {
           title: const Text("DeliMeal"),
         ),
         body: GridView(
-          padding: const EdgeInsets.all(25),
+          padding: const EdgeInsets.all(22),
           children: DUMMY_CATEGORIES
-              .map((catData) => CategoryItem(catData.title, catData.color))
+              .map((catData) =>
+                  CategoryItem(catData.id, catData.title, catData.color))
               .toList(),
           gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 200,
